c5539467c8282a7f3c40ecef632c51f9abeddb54
HarryHoney <harpreetsingh@Harpreets-MacBook-Air.local>
2026-02-25 19:47:19 +0530

    Making the code work

--- news-fetcher/internal/fetcher/worker.go
+++ news-fetcher/internal/fetcher/worker.go
@@ -9,27 +9,13 @@ import (
 )
 
 func StartFetching(ctx context.Context, p provider.NewsProvider, interval time.Duration) {
-	// Rate limiting: ticker ensures we only call every X seconds
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
+	articles, err := p.FetchLatest(ctx)
+	if err != nil {
+		log.Printf("Error or Rate Limit: %v", err)
+	}
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-ticker.C:
-				articles, err := p.FetchLatest(ctx)
-				if err != nil {
-					log.Printf("Error or Rate Limit: %v", err)
-					continue
-				}
-
-				fmt.Printf("Fetched %d normalized articles\n", len(articles))
-				for _, a := range articles {
-					log.Printf("Processed: %s", a.Title)
-				}
-			}
-		}
-	}()
+	fmt.Printf("Fetched %d normalized articles\n", len(articles))
+	for _, a := range articles {
+		log.Printf("Processed: %s", a.Title)
+	}
 }
--- news-fetcher/main.go
+++ news-fetcher/main.go
@@ -2,29 +2,48 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"news-fetcher/internal/fetcher"
 	"news-fetcher/internal/provider"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-
+	apiKey, err := loadAPIKey()
+	if err != nil {
+		panic(err)
+	}
 	// Initialize Provider (Easy to swap in future)
 	p := &provider.NewsDataIO{
-		APIKey: "YOUR_API_KEY",
+		APIKey: apiKey.(string),
 		URL:    "https://newsdata.io/api/1/latest",
 	}
 
 	// Start fetcher in a goroutine with a 10-second rate limit
-	fetcher.StartFetching(ctx, p, 10*time.Second)
+	fetcher.StartFetching(context.Background(), p, 10*time.Second)
+}
 
-	// Keep main alive until interrupted
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-	<-stop
-	cancel()
+func loadAPIKey() (any, any) {
+	// 1. Open the file
+	file, err := os.Open("../creds/api_keys.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// 2. Decode into a map
+	var data map[string]any
+	if err := json.NewDecoder(file).Decode(&data); err != nil {
+		panic(err)
+	}
+
+	// 3. Extract your specific attribute (e.g., "target_id")
+	// We use "type assertion" to ensure it's the type we expect
+	if val, ok := data["newsdata_api_key"]; ok {
+		return val, nil
+	} else {
+		return nil, fmt.Errorf("key not found")
+	}
 }
